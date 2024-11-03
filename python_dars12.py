90c2d6052d37e761b8b64764aa59bec6a72b5307
karl <karl@kali-desktop>
2024-11-03 23:29:12 +0500

    darslariga 12-dars qo'shildi

---
+++ python_dars12.py
@@ -0,0 +1,74 @@
+# 3.11.2024
+# vohidov
+# LUG'AT ELEMENTLARI BILAN ISHLASH
+
+# student = {
+#     'name':'shoxrux',
+#     'lastname':'vohidov',
+#     'age':19,
+#     'Faculty':'computer engineering',
+#     'course':2
+# }
+# print(student.items())
+
+# for key, value in student.items():
+#     print(f"Key: {key.title()}")
+#     print(f"Value {value}\n")
+
+# products = {
+#     'apple' : 10000,
+#     'nut':20000,
+#     'meat' : 30000,
+#     'pomegranate' : 29000,
+#     'tangerine' : 31000,
+#     'pear' : 2000
+# }
+
+# print(products.keys())
+
+# print('Do\'kondagi mahsulotlar: ')
+# for mahsulot in products.keys():
+#     print(f"{mahsulot.title()}")
+
+# for mahsulot in products:
+#     print(f"{mahsulot.title()}")
+
+# bozorlik = ['apple', 'grape', 'pineapple', 'nut']
+
+# for mahsulot in products:
+#     if mahsulot in bozorlik:
+#         print(f"{mahsulot.title()} {products[mahsulot]} $")
+
+# for buyum in bozorlik:
+#     if buyum not in products:
+#         print(f"Iltmos, do'kong {buyum} ham olib keling")
+
+# for mahsulot in sorted(products):
+#     print(mahsulot.title())
+
+# phones = {
+#     '1ali':'iphone x',
+#     'vali':'iphone 11 pro',
+#     '1xasan':'iphone se',
+#     'xusan':'iphone 13 pro max',
+#      '2ali':'iphone x',
+#     'vali':'iphone 11 pro',
+#     '2xasan':'iphone se',
+#     'xusan':'iphone 13 pro max',
+#      '3ali':'iphone x',
+#     'vali':'iphone 11 pro',
+#     '3xasan':'iphone se',
+#     'xusan':'iphone 13 pro max',
+#      '4ali':'iphone x',
+#     'vali':'iphone 11 pro',
+#     '4xasan':'iphone se',
+#     'xusan':'iphone 13 pro max',
+# }
+# print("Telefonlar jamlanmasi:")
+# for tel in set(phones.values()):
+#     print(tel)
+
+# elements = {'11','12','12','13','14','11','13','14'}
+# print(type(elements))
+
+# Amalayot
