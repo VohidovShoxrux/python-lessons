71972663fd97bdad0064de9bd89623ec4c271a08
karl <karl@kali-desktop>
2024-11-06 17:24:30 +0500

    12-darsga amalyoti qo'shildi

--- python_dars12.py
+++ python_dars12.py
@@ -72,3 +72,78 @@
 # print(type(elements))
 
 # Amalayot
+
+# lugat = {
+#     'adryu':'robertson',
+#     'karlo':'anchelotti',
+#     'cristiano':'ranaldo',
+#     'cross':'benjamon',
+#     'bahodir':'jalolov'
+# }
+
+# for key , value in sorted(lugat.items()):
+#     print(f"{key.title()}-{value.title()}")
+   
+# countries = {
+#     "uzbekistan":"Tashkent",
+#     "russia":"Moscow",
+#     "united states":"Washington",
+#     "china":"Beijing",
+#     "france":"Paris",
+#     "united kingdom":"London",
+#     "germany":"Berlin",
+#     "japan":"Tokyo",
+#     "india":"New Delhi",
+#     "turkey":"Ankara",
+# }
+
+# print("Dunyo davlatlari:")
+# for k in sorted(countries):
+#     print(k.upper())
+
+# print('\nDavlatlarning poytaxtlari:')
+# for v in sorted(countries.values()):
+#     print(v.title())
+
+# country = input("What country do you want to know the capital of?:").lower()
+# capital = countries.get(country)
+
+# 1-yechim
+# if capital == None:
+#     print("Bizda bunday ma'lumot yo'q")
+# else:
+#     print(f"{country.upper()}ning poytaxti {capital.title()} shaxri")
+
+# 2- yechim
+# search = input("What country do you want to know the capital of?: ").lower()
+# found = False  # Kalitni topilganligini belgilovchi o'zgaruvchi
+
+# for country, capital in countries.items():
+#     if search == country:  # Foydalanuvchi kiritgan davlatni tekshirish
+#         print(f"{country.upper()}ning poytaxti {capital.title()}!")
+#         found = True  # Kalit topilganda belgilash
+#         break  # Qidirishni to'xtatish
+
+# if not found:
+#     print("Bizda bunday ma'lumot yo'q")
+
+menu = {
+        'osh':20000,
+        "lag'mon":22000,
+        'non':4000,
+        'choy':5000,
+        'shashlik':12000,
+        'somsa':6000,
+        'tabaka':15000
+        }
+
+print('3 ta taom buyurtma bering.')
+orders = []
+for n in range(3):
+    orders.append(input(f"{n+1}-taom:").lower())
+
+for order in orders:
+    if order in menu:
+        print(f"{order.title()} {menu[order]} so'm ")
+    else:
+        print(f"Kechirasiz, bizda {order} yo'q")
