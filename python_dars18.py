eab23b3e68627d6e625853eeb1bacc4575a8deed
karl <karl@kali-desktop>
2024-12-12 17:27:40 +0500

    18-dars qoshildi

---
+++ python_dars18.py
@@ -0,0 +1,46 @@
+# 12.12.2024
+# vohidov
+# FUNKSIYA VA RO'YXAT
+
+# def bahola(ismlar):
+#     baholar = {}
+#     while ismlar:
+#         ism = ismlar.pop() 
+#         baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+#         baholar[ism] = int(baho)
+#     return baholar
+
+# talabalar = ['shoxrux', 'olim', 'anvar', 'hakimjon', 'abdulvaqqos']
+
+# baholar = bahola(talabalar[:])
+# print(baholar)
+
+# Amaliyot
+
+# def katta_harf(royxat):
+#     royxat = royxat[:]
+#     for i in range(len(royxat)):
+#         royxat[i] = royxat[i].title()
+#     return royxat
+
+# ismlar = ['ali', 'vali', 'hasan', 'husan']
+# newlist = katta_harf(ismlar)
+# print(ismlar)
+# print(newlist)
+
+# def bahola(ismlar):
+#     baholar = {}
+#     for ism in ismlar:
+#         baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+#         baholar[ism] = baho
+#     return baholar
+
+# names = ['salim', 'hakim', 'najim', 'jonibek']
+# baholar = bahola(names)
+# print(baholar)
+# print(names)
+
+
+
+
+
