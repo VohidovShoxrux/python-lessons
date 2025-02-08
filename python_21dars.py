29048a7d2247f70db634b634f1048a4c818843df
VohidovShoxrux <vohidovmillatumidvori01@gmail.com>
2025-02-08 23:18:20 +0500

    21-dars qo'shildi

--- python_21dars.py
+++ python_21dars.py
@@ -26,3 +26,40 @@
 
 # avto1 = avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
 # info_print(avto1)
+
+# import math
+
+# x=400
+# print(math.sqrt(x))
+
+# print(pow(5,5))
+
+# from math import pi
+# print(pi)
+
+# import math
+
+# print(math.log2(8))
+# print(math.log10(100))
+
+# import random as r # random modulini r deb chaqirayapmiz
+
+# son = r.randint(0,100) # 0 va 100 oralig'ida tasodifiy son
+# print(son)
+
+# import random as r
+
+# ismlar = ['olim','anvar','hasan','husan']
+# ism = r.choice(ismlar) # ismlar dan tasodifiy ism tanlaymiz
+# print(ism)
+# print(r.choice(ism)) # ismdan tasodifiy harf tanlaymiz
+
+# x = list(range(0,51,5))
+# print(x)
+# print(r.choice(x))
+
+# x = list(range(11))
+# print(x)
+# r.shuffle(x)
+# print(x)
+
