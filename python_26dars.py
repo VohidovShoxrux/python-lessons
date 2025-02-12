687059c49f9a084f7e25baeb7a9e3d07245102a2
VohidovShoxrux <vohidovmillatumidvori01@gmail.com>
2025-02-12 11:27:57 +0500

    26-dars qo'shildi

--- python_26dars.py
+++ python_26dars.py
@@ -3,12 +3,33 @@
 # kiril lotiun telegram bot
 
 from transliterate import to_cyrillic , to_latin
+import telebot 
 
-matn = input("Matn kiriting:")
 
-if matn.isascii():
-    print(to_cyrillic(matn))
-else:
-    print(to_latin(matn))
+TOKEN = '8041947638:AAFStPB_5wPsEqB7yXdQQpGphQ3jxc1cR8Q'
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    javob = "Assalomu alaykum, Xush kelibsiz!"
+    javob += "\nMatn kiriting:"
+    bot.reply_to(message, javob)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    msg = message.text
+    javob = (lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg))(msg)
+    bot.reply_to(message, javob)
+
+bot.polling()
+
+# matn = input("Matn kiriting:")
+
+# if matn.isascii():
+#     print(to_cyrillic(matn))
+# else:
+#     print(to_latin(matn))
 
 
