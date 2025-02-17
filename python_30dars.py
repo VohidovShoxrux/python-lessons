418d884b96d4e898732ebc4129f06fc4d5bfeec7
VohidovShoxrux <vohidovmillatumidvori01@gmail.com>
2025-02-17 08:04:46 +0500

    30-dars qo'shildi

---
+++ python_30dars.py
@@ -0,0 +1,11 @@
+# 17.02.2025
+# vohidov
+# INKAPSULYATSIA, KLASSNING XUSUSIYAT VA METODLARI
+
+# from transport import Avto, Bus, PI
+# # import transport # modulni to'liq import qilish bunda modul nomi. bilan sihlatilinadi
+# # from transport import * # bunday qilish tafsiya qilinmaydi kop hollarda
+
+# avto = Avto("GM","Malibu","Qora",2020,40000)
+
+# amaliyot
